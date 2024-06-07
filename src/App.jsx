498e5018dce4cf04d0be14d1b39d1017cac3a2d0
HARSH <harshnew2204@gmail.com>
2024-06-07 07:50:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
         style={{
             backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
         }}
-    >
+    >Harsh
         <div className="w-full">
             <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
                 <form
